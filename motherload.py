fb0a3cf6e671229a99de7bb9d2e8cde28723bb51
willem.thorbecke@students.olin.edu <willem.thorbecke@students.olin.edu>
2016-03-06 21:28:23 -0500

    finite map to left and right

--- motherload.py
+++ motherload.py
@@ -71,8 +71,9 @@ class BrickBreakerModel(object):
         self.BRICK_HEIGHT = 40
         self.SCREEN_WIDTH = 640
         self.SCREEN_HEIGHT = 480 
-        self.FAR_LEFT = -200 
-        self.FAR_RIGHT = 800
+        self.FAR_LEFT = 320 
+        self.FAR_RIGHT = 320
+        self.FAR_BOTTOM = -680
 
       
         self.init_height_dist = 34
@@ -93,31 +94,42 @@ class BrickBreakerModel(object):
     	
 
     	if what_side == "left":
+    		pass
     		
-    		for top in range (len(self.world)):
-        		for left in range(0,5):
-        			brick = Brick(self.FAR_LEFT - self.BRICK_WIDTH*(left+1), top, self.BRICK_WIDTH, self.BRICK_HEIGHT, False)
-        			self.world[top].insert(0,brick)
+    		# for top in range (len(self.world)):
+      #   		for left in range(0,5):
+      #   			brick = Brick(self.FAR_LEFT - self.BRICK_WIDTH*(left+1), top, self.BRICK_WIDTH, self.BRICK_HEIGHT, False)
+      #   			self.world[top].insert(0,brick)
 
-        	self.temp_world = self.world[:][0:34]
-        	self.FAR_LEFT = self.world[0][4].left #makes the threshold for creating more leftward blocks at 5 blocks from the leftmost column of blocks
+      #   	self.temp_world = self.world[:][0:34]
+      #   	self.FAR_LEFT = self.world[0][4].left #makes the threshold for creating more leftward blocks at 5 blocks from the leftmost column of blocks
 
 
     	elif what_side == "right":
-    		print "ohhh noooo tooo right!!!"
-    		for top in range (len(self.world)):
-        		for right in range(0,5):
+    		pass
+    	
+    		# for top in range (len(self.world)):
+      #   		for right in range(0,5):
+      #   			brick = Brick(self.FAR_RIGHT + self.BRICK_WIDTH*(right+1), top, self.BRICK_WIDTH, self.BRICK_HEIGHT, False)
+      #   			self.world[top].append(brick)
+
+      #   	start_right = len(self.world[0]) - 34
+
+      #   	self.temp_world = self.world[:][start_right:-1]
+      #   	self.FAR_RIGHT = self.world[0][-5].left #makes the threshold for creating more rightward blocks at 5 blocks from the rightmost column of blocks
+      #   	print "new self.right ", self.FAR_RIGHT
+
+
+        elif what_side == "down":
+        	for top in range (len(self.world)):
+    			for right in range(0,5):
         			brick = Brick(self.FAR_RIGHT + self.BRICK_WIDTH*(right+1), top, self.BRICK_WIDTH, self.BRICK_HEIGHT, False)
         			self.world[top].append(brick)
-
         	start_right = len(self.world[0]) - 34
 
         	self.temp_world = self.world[:][start_right:-1]
         	self.FAR_RIGHT = self.world[0][-5].left #makes the threshold for creating more rightward blocks at 5 blocks from the rightmost column of blocks
         	print "new self.right ", self.FAR_RIGHT
-
-
-    	
 
 
 class PyGameKeyboardController(object):
@@ -144,36 +156,43 @@ class PyGameKeyboardController(object):
         		for left in range(len(self.model.temp_world[top])):
         			brick = self.model.temp_world[top][left]
         			brick.top -= brick.height
-        #	print self.model.bricks[0][0].top
+        print self.model.temp_world[0][0].top
 
            
 
         if event.key == pygame.K_LEFT:
+	        if self.model.temp_world[0][0].left != self.model.FAR_LEFT: 
+	        	
 
-        	for top in range(len(self.model.temp_world)):
-        		for left in range(len(self.model.temp_world[top])):
-        			brick = self.model.temp_world[top][left]
-        			brick.left += brick.width
+	        	for top in range(len(self.model.temp_world)):
+	        		for left in range(len(self.model.temp_world[top])):
+	        			brick = self.model.temp_world[top][left]
+	        			brick.left += brick.width
+	        else:
+	        	return
 
         	#if farthest block to left reachest threshold, add more blocks to left
         	if self.model.temp_world[0][0].left == self.model.FAR_LEFT: 
         		self.model.world_enlarger("left")
 
-        	print "left, ", self.model.temp_world[0][0].left	
-        	print "right, ", self.model.temp_world[0][-1].left	
+        	# print "left, ", self.model.temp_world[0][0].left	
+        	# print "right, ", self.model.temp_world[0][-1].left	
 
         	
 
         if event.key == pygame.K_RIGHT:
-        	for top in range(len(self.model.temp_world)):
-        		for left in range(len(self.model.temp_world[top])):
-        			brick = self.model.temp_world[top][left]
-        			brick.left -= brick.width
+        	if self.model.world[0][-1].left != self.model.FAR_RIGHT: 
+	        	for top in range(len(self.model.temp_world)):
+	        		for left in range(len(self.model.temp_world[top])):
+	        			brick = self.model.temp_world[top][left]
+	        			brick.left -= brick.width
+	        else:
+	        	return
         	if self.model.world[0][-1].left == self.model.FAR_RIGHT: 
         		self.model.world_enlarger("right")
 
-        	print "left, ", self.model.temp_world[0][0].left	
-        	print "right, ", self.model.temp_world[0][-1].left	
+        	# print "left, ", self.model.temp_world[0][0].left	
+        	# print "right, ", self.model.temp_world[0][-1].left	
         
 
 
